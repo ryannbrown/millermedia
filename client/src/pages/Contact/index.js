2dcf8617fde5359dda6d1244970f05a36d4f26f8
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-10-03 13:50:59 -0400

    attempting new way to render map

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -15,12 +15,12 @@ import "./style.css";
 import ContactForms from "../../components/ContactForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import Hero from '../../components/Hero'
-import heroImg from "../../media/bread.jpg"
-import Quote from '../../components/Quote/quote.js'
-import Ppcblock from '../Services/components/ppcblock/index'
+import Hero from "../../components/Hero";
+import heroImg from "../../media/bread.jpg";
+import Quote from "../../components/Quote/quote.js";
+import Ppcblock from "../Services/components/ppcblock/index";
 // import arrow from "../../../media/white-arrow.png"
-import Footer from '../../components/Footer/footer'
+import Footer from "../../components/Footer/footer";
 // import Navigation from "../../components/Navigation"
 // import logo from "../../media/moons.png";
 // import blueLogo from "../../media/bluemoons.png";
@@ -34,29 +34,38 @@ require("dotenv").config();
 export default class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      renderMap: false,
+    };
   }
 
-//   componentDidMount() {
-    // const mapKey = process.env.REACT_APP_MAP;
-//     var script = document.createElement('script');
-//     script.src = `https://maps.googleapis.com/maps/api/js?key=${mapKey}&callback=initMap`;
-//     script.defer = true;
-    
-//     // Attach your callback function to the `window` object
-//     window.initMap = function() {
-//       // JS API is loaded and available
-      
-//     };
-    
-    
-//     // Append the 'script' element to 'head'
-//     document.head.appendChild(script);
-//   }
+  //   componentDidMount() {
+  // const mapKey = process.env.REACT_APP_MAP;
+  //     var script = document.createElement('script');
+  //     script.src = `https://maps.googleapis.com/maps/api/js?key=${mapKey}&callback=initMap`;
+  //     script.defer = true;
+
+  //     // Attach your callback function to the `window` object
+  //     window.initMap = function() {
+  //       // JS API is loaded and available
+
+  //     };
+
+  //     // Append the 'script' element to 'head'
+  //     document.head.appendChild(script);
+  //   }
+
+  componentDidMount() {
+    const renderMap = () => {
+      const mapKey = process.env.REACT_APP_MAP;
+      this.setState({ renderMap: true });
+    };
+    renderMap();
+  }
 
   render() {
     const mapKey = process.env.REACT_APP_MAP;
-  
+
     return (
       <div className="connect-page">
         <Hero
@@ -68,31 +77,39 @@ If a company were only to run one type of advertising, we would insist it be PPC
           button="SCROLL FOR MORE"
         />
         <div className="home-wrapper">
-         <p>Interested in our PPC Advertising, Facebook Ads, or Graphic Design services or simply want more information on Miller Media Partners? Give us a call or complete the form below.</p>
-         <div className="contact-boxes">
-          <ContactForms></ContactForms>
-          <div className="info-hours">
+          <p>
+            Interested in our PPC Advertising, Facebook Ads, or Graphic Design
+            services or simply want more information on Miller Media Partners?
+            Give us a call or complete the form below.
+          </p>
+          <div className="contact-boxes">
+            <ContactForms></ContactForms>
+            <div className="info-hours">
               <div>
-                  <h1>Contact Info</h1>
-                  <p>info@millermediapartners.com </p>
-                  <p>919.229.9095</p>
+                <h1>Contact Info</h1>
+                <p>info@millermediapartners.com </p>
+                <p>919.229.9095</p>
               </div>
               <div>
-                  <h1>Hours of Operation</h1>
-                  <p>Monday - Friday</p>
-                  <p>8am-4pm</p>
+                <h1>Hours of Operation</h1>
+                <p>Monday - Friday</p>
+                <p>8am-4pm</p>
               </div>
+            </div>
           </div>
-         </div>
         </div>
+        {this.state.renderMap && (
+          <iframe
+            width="100%"
+            height="350"
+            frameborder="0"
+            style={{ border: "0" }}
+            src={`https://www.google.com/maps/embed/v1/place?key=${mapKey}&q=Sarasota,Fl&zoom=15`}
+            allowfullscreen
+          ></iframe>
+        )}
         <div className="map-div">
-<iframe
-  width="100%"
-  height="350"
-  frameborder="0" style={{border:"0"}}
-  src={`https://www.google.com/maps/embed/v1/place?key=${mapKey}&q=Sarasota,Fl&zoom=15`} allowfullscreen>
-</iframe>
-<div id="map"></div>
+          <div id="map"></div>
         </div>
         <Footer
           text="Looking for more info?"
